557625f0c192d55137bcf438357b73c2ebf1c5a3
Hrudayangam Mehta <51739491+Hrudayangam@users.noreply.github.com>
2023-10-24 14:27:52 -0400

    Time: 26 ms (87.32%) | Memory: 13.7 MB (79.85%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,14 +1,14 @@
-class Solution:
-    def isAnagram(self,s,t):
-        if len(s)!= len(t):
-            return False
+# class Solution:
+#     def isAnagram(self,s,t):
+#         if len(s)!= len(t):
+#             return False
         
-        count_s,count_t = {},{}
+#         count_s,count_t = {},{}
 
-        for i in range(len(s)):
-            count_s[s[i]] = 1 + count_s.get(s[i],0)
-            count_t[t[i]] = 1 + count_t.get(t[i],0)
-        return count_s == count_t
+#         for i in range(len(s)):
+#             count_s[s[i]] = 1 + count_s.get(s[i],0)
+#             count_t[t[i]] = 1 + count_t.get(t[i],0)
+#         return count_s == count_t
 
     
 
@@ -16,34 +16,34 @@ class Solution:
 
 
 
-# class Solution(object):
-#     def isAnagram(self, s, t):
-#         """
-#         :type s: str
-#         :type t: str
-#         :rtype: bool
-#         """
-#         count_s={}
-#         count_t={}
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        count_s={}
+        count_t={}
 
-#         for i in s:
-#             if i not in count_s:
-#                 count_s[i] = 1
-#             # print(count_s)
+        for i in s:
+            if i not in count_s:
+                count_s[i] = 1
+            # print(count_s)
 
-#             else:
-#                 count_s[i] +=1
+            else:
+                count_s[i] +=1
 
-#         for j in t:
-#             if j not in count_t:
-#                 count_t[j] = 1
-#             else:
-#                 count_t[j] +=1
+        for j in t:
+            if j not in count_t:
+                count_t[j] = 1
+            else:
+                count_t[j] +=1
         
-#         if  count_t== count_s:
-#             return True
-#         else:
-#            return False
+        if  count_t== count_s:
+            return True
+        else:
+           return False
 
 
 
